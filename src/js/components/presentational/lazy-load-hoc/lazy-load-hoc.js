f82ae59751a96e6747d4986738b9158dd050f937
Illés Alexander <illes.alexander88@gmail.com>
2019-11-21 21:13:58 +0100

    scroll experience improved

--- src/js/components/presentational/lazy-load-hoc/lazy-load-hoc.js
+++ src/js/components/presentational/lazy-load-hoc/lazy-load-hoc.js
@@ -1,25 +1,28 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, Suspense } from "react";
 import useOnScreen from "../../../hooks/useScreen";
 
 export const withLazyLoad = (Component, id, color = "#265C42") => {
   return props => {
     const ref = useRef(null);
-    const [load, setLoad] = useState(false)
-    const [height, setHeight] = useState("100vh")
+    const [load, setLoad] = useState(false);
+    const [height, setHeight] = useState("100vh");
     const isOnScreen = useOnScreen(ref, 0.5);
     const style = {
       width: "100%",
       height: height,
       backgroundColor: color
     };
-    if(isOnScreen && !load){
+    if (isOnScreen && !load) {
       setLoad(true);
-      setHeight("auto")
+      setHeight("auto");
     }
     return (
       <div id={id} className="lazy-load-wrapper" style={style} ref={ref}>
-        { load ?
-        <Component /> : null }
+        {load ? (
+          <Suspense fallback={<div></div>}>
+            <Component />
+          </Suspense>
+        ) : null}
       </div>
     );
   };
