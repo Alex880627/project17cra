37c19bd0de272cfa4ebafe8def568aec2a937e06
Illés Alexander <illes.alexander88@gmail.com>
2019-12-11 21:18:29 +0100

    updated admin service

--- src/js/components/presentational/cover-pic/cover-pic.js
+++ src/js/components/presentational/cover-pic/cover-pic.js
@@ -1,13 +1,22 @@
 import React from "react";
 import pic7 from "../../../../data/images/pic7.jpg";
-import './cover-pic.css';
+import "./cover-pic.css";
 
-const CoverPic = (props) => {
+const CoverPic = props => {
+  const screenRatio = () => window.innerWidth / window.innerHeight;
+
+  const isBigSreen = () => window.innerWidth / window.innerHeight > 1.765;
+
   return (
-    <div
-      className={`cover-img ${props.blur? "blur-img":""}`}
-    >
-      <img src={pic7}  style={{ width: window.screen.width}}/>
+    <div className={`cover-img ${props.blur ? "blur-img" : ""}`}>
+      <img
+        src={pic7}
+        style={
+          isBigSreen
+            ? { width: window.screen.width }
+            : { width: window.screen.height * screenRatio() }
+        }
+      />
     </div>
   );
 };
